f24c742305e798362e706275f4738f3db1481018
Raffaele Rasini <raffaele.rasini@gmail.com>
2024-10-23 16:01:40 +0200

    added error

--- src/js/plugin/plugin.ts
+++ src/js/plugin/plugin.ts
@@ -4,6 +4,9 @@ import { Splide, SplideSlide } from '../components';
 
 export const VueSplide = {
   install( app: App ): void {
+    if ( !Splide.name || !SplideSlide.name ) {
+      throw new Error( 'VueSplide components are not installed. Please check the installation guide.' );
+    }
     app.component( Splide.name, Splide );
     app.component( SplideSlide.name, SplideSlide );
   },
